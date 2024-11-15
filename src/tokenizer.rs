c484739ab990e85f67c05c3a23019da41f7d559d
Daegyeom Ha <combbm@gmail.com>
2024-11-15 14:36:09 +0900

    codecrafters submit [skip ci]

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -3,9 +3,16 @@ use std::io::{self, Write};
 pub fn tokenize(input: &str) -> i32 {
     let mut line = 1;
     let mut latest_error_code = 0;
+    let mut found_single_line_comment = false;
     let mut chars = input.chars().peekable();
 
     while let Some(char) = chars.next() {
+        if found_single_line_comment {
+            if char == '\n' {
+                found_single_line_comment = false;
+            }
+            continue;
+        }
         match char {
             '(' => println!("LEFT_PAREN ( null"),
             ')' => println!("RIGHT_PAREN ) null"),
@@ -47,6 +54,13 @@ pub fn tokenize(input: &str) -> i32 {
                     println!("LESS_EQUAL <= null");
                 } else {
                     println!("LESS < null");
+                }
+            }
+            '/' => {
+                if let Some(&'/') = chars.peek() {
+                    found_single_line_comment = true;
+                } else {
+                    println!("SLASH / null");
                 }
             }
             '\n' => line += 1,
